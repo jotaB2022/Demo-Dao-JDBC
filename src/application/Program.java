df2ca34e8e89dbd731654d5e342cecaf5216a87d
jotaB <joaobatista1999estudo@gmail.com>
2023-04-06 22:30:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,11 @@ public class Program {
 		Department obj = new Department(10, "Lucas" );
 		
 		Seller seller = new Seller(2,"Rafael","rafael@gmail",new Date(), 300.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
+		
 		
 		System.out.println(seller);
 	}
